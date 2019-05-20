9608621d54eb228988025589e3e6bb3786d829ed
李艳杰 <liyanjie@belle.com.cn>
2019-05-20 19:56:06 +0800

    Initial commit

---
+++ com/lijie/excel/excel_handle.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+import os
+from datetime import datetime
+
+import xlrd
+from xlrd import xldate_as_tuple
+
+from com.lijie.oracle.oracle_handle import *
+
+# 解决乱码问题
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+
+
+# xlrd.Book.encoding = "gbk"
+
+# ctype： 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+class excelHandle:
+    def decode(self, filename, sheetname):
+        try:
+            filename = filename.decode('utf-8')
+            sheetname = sheetname.decode('utf-8')
+        except Exception:
+            print(traceback.print_exc())
+        return filename, sheetname
+
+    def read_excel(self, filename, sheetname):
+        # filename, sheetname = self.decode(filename, sheetname)
+        rbook = xlrd.open_workbook(filename)
+        sheet = rbook.sheet_by_name(sheetname)
+        rows = sheet.nrows
+        cols = sheet.ncols
+        all_content = []
+        for i in range(rows):
+            row_content = []
+            for j in range(cols):
+                ctype = sheet.cell(i, j).ctype  # 表格的数据类型
+                cell = sheet.cell_value(i, j)
+                if ctype == 2 and cell % 1 == 0:  # 如果是整形
+                    # cell = int(cell)
+                    pass
+                elif ctype == 3:
+                    # 转成datetime对象
+                    date = datetime(*xldate_as_tuple(cell, 0))
+                    cell = date.strftime('%Y/%d/%m %H:%M:%S')
+                    # cell = "to_date("+date.strftime('%Y/%d/%m %H:%M:%S')+",'yyyy-mm-dd hh24:mi:ss')"
+                elif ctype == 4:
+                    cell = True if cell == 1 else False
+                row_content.append(cell)
+            all_content.append(tuple(row_content))
+            # print ('[' + ','.join("'" + str(element) + "'" for element in row_content) + ']')
+        # del all_content[0]
+
+        return all_content
+
+
+if __name__ == '__main__':
+    eh = excelHandle()
+    filename = r'E:686/3.xlsx'
+    sheet_name = 'A库'
+    excel_data = eh.read_excel(filename, sheet_name)
+    table_name = "cont_test2"
+    connect = oracleHandle.connect("u_db_wx", "u_db_wx", "172.17.209.243:1521/bidbportal")
+    oracleHandle.insert(connect, excel_data, table_name)
---
+++ com/lijie/oracle/oracle_handle.py
@@ -0,0 +1,40 @@
+import cx_Oracle
+import traceback
+import re
+
+
+class oracleHandle:
+    def connect(username, password, url):
+        # conn = cx_Oracle.connect("u_db_wx", "u_db_wx", "172.17.209.243:1521/bidbportal")
+        conn = cx_Oracle.connect(username, password, url)
+        return conn
+
+    def insert(connection, data, tableName):
+        # connect oracle database
+        # db = cx_Oracle.connect("u_db_wx", "u_db_wx", "172.17.209.243:1521/bidbportal")
+        cursor = connection.cursor()
+        fields = data.pop(0)
+        fields_str = ",".join(fields)
+        execute_sql = ""
+        execute_sql += "insert into %s ( %s) values (" % (tableName, fields_str)
+        count = 1
+        for field in fields:
+            if count < len(fields):
+                if re.search("date", field, re.IGNORECASE):
+                    execute_sql += ",to_date(:" + str(count) + ",'yyyy-mm-dd hh24:mi:ss')"
+                elif count != 1:
+                    execute_sql += ",:" + str(count)
+                elif count == 1:
+                    execute_sql += ":" + str(count)
+            elif count == len(fields):
+                execute_sql += ",:" + str(count) + ")"
+            count += 1
+        print(execute_sql)
+        cursor.prepare(execute_sql)
+        try:
+            cursor.executemany(None, data)
+        except Exception:
+            print(traceback.print_exc())
+        connection.commit()
+        cursor.close()
+        connection.close()
